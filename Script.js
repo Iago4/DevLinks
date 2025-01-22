a6ba946669315fef40e1757001ac7f0dd7c2daa0
Iago <iago12anosbastos@gmail.com>
2025-01-22 11:41:11 -0300

    a

--- Script.js
+++ Script.js
@@ -1,4 +1,5 @@
 function toggleMode() {
+  const sound = document.getElementById("som")
   const html = document.documentElement
   html.classList.toggle("light")
 
@@ -8,11 +9,14 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute("alt", "shinji feliz")
+    img.setAttribute("alt", "goku")
+    
     //se estiver light mode adicionar a imagem light
   } else {
     img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute("alt", "shinji triste")
+    img.setAttribute("alt", "goku dark")
+    sound.currentTime = 0
+    sound.play()
     //se estiver sem light mode manter a imagem normal
   }
 }
